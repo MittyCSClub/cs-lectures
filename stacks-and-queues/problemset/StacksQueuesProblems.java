f3d553252a7c63f1ef49521cf9dc7ec26891ed2c
TanayB11 <tanaybiradar24@gmail.com>
2021-01-28 21:31:53 -0800

    Add output demarcation

--- stacks-and-queues/problemset/StacksQueuesProblems.java
+++ stacks-and-queues/problemset/StacksQueuesProblems.java
@@ -3,6 +3,7 @@ import java.util.*;
 
 public class StacksQueuesProblems {
   public static void main(String[] args) throws FileNotFoundException, IOException {
+    // Screenshot output and code -> Canvas
 
     // Problem 1: Reverse
     // Read from the file "problemone.txt" and print its contents
@@ -21,6 +22,7 @@ public class StacksQueuesProblems {
     // current number and the previous two numbers using a queue
 
     sc = new Scanner(new File("problemtwo.txt"));
+    System.out.println("Problem 2 Output\n----------------");
     while (sc.hasNextLine()) {
       line = sc.nextLine();
       // Write your code here
