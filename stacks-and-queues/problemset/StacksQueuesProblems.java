b8f433ded98faea63ae8e66b5b21b89060f7a298
TanayB11 <tanaybiradar24@gmail.com>
2021-01-28 21:29:54 -0800

    Format comments on problemset

--- stacks-and-queues/problemset/StacksQueuesProblems.java
+++ stacks-and-queues/problemset/StacksQueuesProblems.java
@@ -4,9 +4,10 @@ import java.util.*;
 public class StacksQueuesProblems {
   public static void main(String[] args) throws FileNotFoundException, IOException {
 
-    // Problem 1: Read from the file "problemone.txt" and print its contents
+    // Problem 1: Reverse
+    // Read from the file "problemone.txt" and print its contents
     // line-by-line in reverse order using a stack
-    //
+
     Scanner sc = new Scanner(new File("problemone.txt"));
     while (sc.hasNextLine()) {
       String line = sc.nextLine(); // reads next line from file
@@ -16,9 +17,9 @@ public class StacksQueuesProblems {
 
 
     // Problem 2: Rolling average
-    // While reading the file "problemtwo.txt," print the average of the
+    // While reading the file "problemtwo.txt" print the average of the
     // current number and the previous two numbers using a queue
-    //
+
     sc = new Scanner(new File("problemtwo.txt"));
     while (sc.hasNextLine()) {
       line = sc.nextLine();
